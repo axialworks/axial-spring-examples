7f31afe3b459431f7f828a622f52af7d1ad35bd6
selman.kaya <kayaselmann@gmail.com>
2023-11-02 16:47:47 +0300

    working openapi added.

---
+++ src/main/java/com/axial/extension/openapi/BaseApiConfig.java
@@ -0,0 +1,9 @@
+package com.axial.extension.openapi;
+
+import com.axial.modules.openapi.ApiCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseApiConfig implements ApiCustomizer {
+
+}
